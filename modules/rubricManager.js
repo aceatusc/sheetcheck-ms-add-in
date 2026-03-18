dafed087c48281667856e38430dc3e8e4df8ed08
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-03-17 20:39:05 -0700

    update : `of`s removed.

--- modules/rubricManager.js
+++ modules/rubricManager.js
@@ -170,8 +170,9 @@ const RubricManager = (() => {
             });
 
             const total = res.results?.length || 0;
+            // ? `${metCount} of ${total} requirement${total !== 1 ? 's' : ''} met`
             _desc.textContent = total
-                ? `${metCount} of ${total} requirement${total !== 1 ? 's' : ''} met`
+                ? `${metCount} met`
                 : 'No requirements to verify.';
 
         } catch (err) {
